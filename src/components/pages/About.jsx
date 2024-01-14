fe007ca543124039d4aaed22516faae9d3855709
Chae-Lee <katelee1129@gmail.com>
2024-01-14 02:21:01 +0000

    About component updated

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import "./About.css";
 
 function About() {
-  return <p> About section </p>;
+  return (
+    <div className="aboutMe">
+      <p id="aboutTitle"> About Me </p>
+      <p>
+        Hello my name is Kate and I am an effective and reliable communicator
+        transitioning from HR to Web development with a unique blend of skills
+        combining HR insights with emerging expertise in HTML, CSS and
+        JavaScript. This is my portfolio where I will be showcasing projects I
+        have been working on. The projects I have completed are shown below. I
+        am a self-motivated individual that is passionate about continuous
+        learning, collaboration and delivering exceptional user experiences.
+        Eager to leverage emerging technical skills in frontend development to
+        contribute innovative solutions and enhance web applications, I will be
+        seeking a position as a Frontend Web Developer
+      </p>
+    </div>
+  );
 }
 
 export default About;
